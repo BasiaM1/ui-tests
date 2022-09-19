bfd60082798a9de4bb5807bd0418b5db1f30b226
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-09-19 18:37:53 +0200

    Set cookie

--- src/main/java/com/basia/utils/LoginUtil.java
+++ src/main/java/com/basia/utils/LoginUtil.java
@@ -5,10 +5,12 @@ import com.basia.api.ApiRegister;
 import com.basia.api.dto.login.LoginDto;
 import com.basia.api.dto.login.LoginResponseDto;
 import com.basia.api.dto.register.RegisterDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.html5.LocalStorage;
 import org.openqa.selenium.html5.WebStorage;
@@ -23,9 +25,18 @@ public class LoginUtil {
     public static String loginAsRandomUser(RegisterDto user, WebDriver driver) {
         apiRegister.register(user);
         LoginResponseDto loginResponse = apiLogin.login(new LoginDto(user.getUsername(), user.getPassword()));
-        String userValue = objectMapper.writeValueAsString(loginResponse);
-        LocalStorage local = ((WebStorage) driver).getLocalStorage();
-        local.setItem("user", userValue);
+        setLocalStorage(driver, loginResponse);
+        setCookie(driver, loginResponse.getToken());
         return loginResponse.getToken();
+    }
+
+    private static void setCookie(WebDriver driver, String token) {
+        driver.manage().addCookie(new Cookie("token", token));
+    }
+
+    private static void setLocalStorage(WebDriver driver, LoginResponseDto loginResponse) throws JsonProcessingException {
+        String userLocalStorageEntry = objectMapper.writeValueAsString(loginResponse);
+        LocalStorage local = ((WebStorage) driver).getLocalStorage();
+        local.setItem("user", userLocalStorageEntry);
     }
 }
